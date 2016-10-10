e3c36701733bf25aaf092c4c05f0ff631417fdd1
Mitz Amano <mitz@linux.com>
2016-10-10 09:53:46 +0700

    Add mediaproxy-relay docker files.

---
+++ mediaproxy-relay/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+DB_ROOT_PASS=${DB_ROOT_PASS:-"password"}
+
+HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
+MP_DISPATCHERS=$HOST_IP:25060
+MP_PORTRANGE=20000:60000
+MP_PERIOD=0
+MP_LISTEN=$HOST_IP:25060
+MP_LISTEN_MANAGEMENT=127.0.0.1:25061
+DB_USER=${DB_USER:-"statistic"}
+DB_PASS=${DB_PASS:-"statistic"}
+DB_HOST=${DB_HOST:-"127.0.0.1"}
+DB=${DB:-"statistic"}
+DB_URI=${DB_URI:-"mysql://$DB_USER:$DB_PASS@$DB_HOST/$DB"}
+
+/usr/bin/media-relay --no-fork
