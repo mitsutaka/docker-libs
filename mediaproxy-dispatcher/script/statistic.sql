4841f93ec4e163bec136641283a41de3a4666932
Mitz Amano <mitz@linux.com>
2016-10-10 09:55:15 +0700

    Add statistic db schema for mediaproxy-dispatcher.

---
+++ mediaproxy-dispatcher/script/statistic.sql
@@ -0,0 +1,68 @@
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
+--
+-- Host: 192.168.170.5    Database: statistic
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `statistic` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `statistic`;
+
+--
+-- Table structure for table `call_log`
+--
+
+DROP TABLE IF EXISTS `call_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `call_log` (
+  `call_id` char(50) NOT NULL,
+  `call_start_time` char(50) DEFAULT NULL,
+  `call_end_time` char(50) DEFAULT NULL,
+  `proxy` char(50) DEFAULT NULL,
+  `caller_id` char(50) DEFAULT NULL,
+  `caller_ip` char(50) DEFAULT NULL,
+  `caller_geo` char(50) DEFAULT NULL,
+  `callee_id` char(50) DEFAULT NULL,
+  `callee_ip` char(50) DEFAULT NULL,
+  `callee_geo` char(50) DEFAULT NULL,
+  KEY `call_id` (`call_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `p2p_duration`
+--
+
+DROP TABLE IF EXISTS `p2p_duration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `p2p_duration` (
+  `call_id` char(50) NOT NULL,
+  `p2p_start_time` int(11) DEFAULT '0',
+  `duration` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-27 22:31:09
