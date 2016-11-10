6dc53aaf037f0061c1327698f2e4895907200bc1
Mitz Amano <mitz@linux.com>
2016-11-10 21:56:14 +0700

    Add new docker image znc

---
+++ znc/entrypoint.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+DATADIR="/znc-data"
+
+if [ -d "${DATADIR}/modules" ]; then
+  cwd="$(pwd)"
+
+  modules=$(find "${DATADIR}/modules" -name "*.cpp")
+
+  for module in $modules; do
+    echo "Building module $module..."
+    cd "$(dirname "$module")"
+    znc-buildmod "$module"
+  done
+
+  cd "$cwd"
+fi
+
+if [ ! -f "${DATADIR}/configs/znc.conf" ]; then
+  echo "Creating a default configuration..."
+  mkdir -p "${DATADIR}/configs"
+  cp /znc.conf.default "${DATADIR}/configs/znc.conf"
+fi
+
+exec sudo -u znc znc --foreground --datadir="$DATADIR" $@
