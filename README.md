fd34b44209ad73331eef97e53fd87455c6d5c28d
Mitz Amano <mitsutaka.amano@gmail.com>
2019-01-14 10:43:41 +0900

    Start tagging all container images

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Build Status](https://travis-ci.org/mitsutaka/docker-libs.svg?branch=master)](https://travis-ci.org/mitsutaka/docker-libs)
 [![CircleCI](https://circleci.com/gh/mitsutaka/docker-libs.svg?style=svg)](https://circleci.com/gh/mitsutaka/docker-libs)
 
-
 # docker-libs
-Docker containers for my project
+
+Docker containers
---
+++ tag_exists.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: tag_exists NAME"
+    exit 1
+fi
+
+NAME="$1"
+TAG=$(cat "$NAME"/TAG)
+TOKEN=$(curl -s \
+    -H 'Accept-Encoding: application/json' \
+    -u "$DOCKER_USER:$DOCKER_PASS" \
+"https://auth.docker.io/token?scope=repository:mitsutaka/$NAME:pull&service=registry.docker.io" | jq -r '.token')
+TAGS=$(curl -s \
+    -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+    -H "Authorization: Bearer $TOKEN" \
+"https://registry-1.docker.io/v2/mitsutaka/$NAME/tags/list" | jq -r '.tags[]')
+
+for t in $TAGS; do
+    if [ "$t" = "$TAG" ]; then
+        echo "ok"
+        exit 0
+    fi
+done
+
+echo "ng"
