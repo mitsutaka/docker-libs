93224c74d7d8f287fd7554c65c077071d8cd7a72
Mitz Amano <mitz@linux.com>
2019-02-16 22:18:42 +0900

    Fix tag_exists.sh

--- tag_exists.sh
+++ tag_exists.sh
@@ -11,10 +11,12 @@ TOKEN=$(curl -s \
     -H 'Accept-Encoding: application/json' \
     -u "$DOCKER_USER:$DOCKER_PASS" \
 "https://auth.docker.io/token?scope=repository:mitsutaka/$NAME:pull&service=registry.docker.io" | jq -r '.token')
+set +e
 TAGS=$(curl -s \
     -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
     -H "Authorization: Bearer $TOKEN" \
 "https://registry-1.docker.io/v2/mitsutaka/$NAME/tags/list" | jq -r '.tags[]')
+set -e
 
 for t in $TAGS; do
     if [ "$t" = "$TAG" ]; then
